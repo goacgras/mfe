1a1975267a6fc8e1a8b74a25a4ba597db0accae5
Fachreza Ghifary <goacgras@gmail.com>
2022-08-19 16:35:54 +0700

    created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -14,22 +16,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
-
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader",
-//           options: {
-//             presets: ["@babel/preset-react", "@babel/preset-env"],
-//             plugins: ["@babel/plugin-transform-runtime"],
-//           },
-//         },
-//       },
-//     ],
-//   },
-// };
--- packages/marketing/config/webpack.common.js
+++ packages/marketing/config/webpack.common.js
@@ -15,21 +15,3 @@ module.exports = {
     ],
   },
 };
-
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader",
-//           options: {
-//             presets: ["@babel/preset-react", "@babel/preset-env"],
-//             plugins: ["@babel/plugin-transform-runtime"],
-//           },
-//         },
-//       },
-//     ],
-//   },
-// };
